2f5aca5ec45f84062d3ca0d00dfc9adc61338605
Bogdan Kalitenko <kalitenkobv@gmail.com>
2025-05-30 17:41:20 +0500

    ImageFeed 9 sprint

---
+++ ImageFeed/Helpers/Color+Resources .swift
@@ -0,0 +1,9 @@
+extension ColorResource {
+    static let ypBackground = ColorResource(name: "YP Background (iOS)", bundle: .main)
+    static let ypBlack = ColorResource(name: "YP Black (iOS)", bundle: .main)
+    static let ypBlue = ColorResource(name: "YP Blue (iOS)", bundle: .main)
+    static let ypGray = ColorResource(name: "YP Gray (iOS)", bundle: .main)
+    static let ypRed = ColorResource(name: "YP Red (iOS)", bundle: .main)
+    static let ypWhiteAlpha50 = ColorResource(name: "YP White (Alpha 50) (iOS)", bundle: .main)
+    static let ypWhite = ColorResource(name: "YP White (iOS)", bundle: .main)
+}
---
+++ ImageFeed/Helpers/UIFont+Extension.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+extension UIFont {
+    static let bold23 = UIFont.systemFont(ofSize: 23, weight: .bold)
+    static let regular13 = UIFont.systemFont(ofSize: 13, weight: .regular)
+}
